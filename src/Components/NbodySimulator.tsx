717bacbdc2ef2de4ee5b61696edfa34bf88178d4
Im-Rises <quentin-morel88@hotmail.com>
2023-05-14 02:03:03 -0400

    wip

--- src/Components/NbodySimulator.tsx
+++ src/Components/NbodySimulator.tsx
@@ -3,7 +3,7 @@ import Sketch from 'react-p5';
 import type p5Types from 'p5';
 import {isMobile} from 'react-device-detect';
 import {NBODY_COUNT_COMPUTER, NBODY_COUNT_MOBILE} from '../Constants/constant-nbody-simulator';
-import type Particle from '../Classes/Particle';
+import Particle from '../Classes/Particle';
 
 type Quadruplet = [number, number, number, number];
 
@@ -44,8 +44,8 @@ const ParticleSimulator: React.FC<ComponentProps> = (props: ComponentProps) => {
 	} = props;
 
 	// Time variables
-	const previousTime = 0;
-	const fixedUpdateAccum = 0;
+	let previousTime = 0;
+	let fixedUpdateAccum = 0;
 	const fixedDeltaTime = 1 / fixedUpdate;
 
 	// P5 variables
@@ -66,49 +66,49 @@ const ParticleSimulator: React.FC<ComponentProps> = (props: ComponentProps) => {
 		// Set frame rate to 60
 		p5.frameRate(frameRate);
 
-		// // Create particles
-		// for (let i = 0; i < (isMobile ? nbodyCountMobile : nbodyCountComputer); i++) {
-		// 	// Define particles spawn in a circle
-		// 	const randomFloat = (min: number, max: number) => min + ((max - min) * Math.random());
-		// 	const randomAngle1 = randomFloat(0, 2 * Math.PI);
-		// 	const randomAngle2 = randomFloat(0, 2 * Math.PI);
-		// 	const posX = (p5.width / 2) + (spawnAreaRadius * Math.cos(randomAngle1) * Math.sin(randomAngle2));
-		// 	const posY = (p5.height / 2) + (spawnAreaRadius * Math.sin(randomAngle1) * Math.sin(randomAngle2));
-		// 	particles.push(new Particle(p5, posX, posY));
-		// }
+		// Create particles
+		for (let i = 0; i < (isMobile ? nbodyCountMobile : nbodyCountComputer); i++) {
+			// Define particles spawn in a circle
+			const randomFloat = (min: number, max: number) => min + ((max - min) * Math.random());
+			const randomAngle1 = randomFloat(0, 2 * Math.PI);
+			const randomAngle2 = randomFloat(0, 2 * Math.PI);
+			const posX = (p5.width / 2) + (spawnAreaRadius * Math.cos(randomAngle1) * Math.sin(randomAngle2));
+			const posY = (p5.height / 2) + (spawnAreaRadius * Math.sin(randomAngle1) * Math.sin(randomAngle2));
+			particles.push(new Particle(p5, posX, posY));
+		}
 	};
 
 	// Sketch draw call every frame (60 fps) game loop
 	const draw = (p5: p5Types) => {
-		// /* Calculate deltaTime and update fixedUpdateAccum */
-		// const currentTime = p5.millis();
-		// const deltaTime = (currentTime - previousTime) / 1000;// in seconds
-		// previousTime = currentTime;
-		// fixedUpdateAccum += deltaTime;
+		/* Calculate deltaTime and update fixedUpdateAccum */
+		const currentTime = p5.millis();
+		const deltaTime = (currentTime - previousTime) / 1000;// in seconds
+		previousTime = currentTime;
+		fixedUpdateAccum += deltaTime;
 
-		// /* Update physics (fixed update) */
-		// if (fixedUpdateAccum >= fixedDeltaTime) {
-		// 	fixedUpdateAccum = 0;
-		//
-		// 	// Update particles
-		// 	for (const particle of particles) {
-		// 		// particle.update(p5, particles, fixedDeltaTime, gravitationalConstant, pixelsPerMeter);
-		// 	}
-		// }
+		/* Update physics (fixed update) */
+		if (fixedUpdateAccum >= fixedDeltaTime) {
+			fixedUpdateAccum = 0;
+
+			// Update particles
+			for (const particle of particles) {
+				// particle.update(p5, particles, fixedDeltaTime, gravitationalConstant, pixelsPerMeter);
+			}
+		}
 
 		/* Update canvas */
 		// Clear canvas
 		screenBuffer.background(backColor[0], backColor[1], backColor[2], backColor[3]);
 
-		// // Draw objects
-		// for (const particle of particles) {
-		// 	particle.show(p5);
-		// }
+		// Draw objects
+		for (const particle of particles) {
+			particle.show(screenBuffer);
+		}
 
-		p5.stroke(100);
-		p5.strokeWeight(1);
-		p5.fill(255, 0, 0);
-		p5.circle(p5.width / 2, p5.height / 2, 100 * 2);
+		// screenBuffer.stroke(100);
+		// screenBuffer.strokeWeight(1);
+		// screenBuffer.fill(255, 0, 0);
+		// screenBuffer.circle(p5.width / 2, p5.height / 2, 100 * 2);
 
 		// Swap buffers
 		p5.image(screenBuffer, 0, 0);
