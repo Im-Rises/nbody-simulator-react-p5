8669d274602d167f24ff78ab2eddef936c7fcaad
Im-Rises <quentin-morel88@hotmail.com>
2023-05-17 02:19:35 -0400

    New version

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import ParticleSimulator from './components/NbodySimulator';
+import NbodySimulator from './components/NbodySimulator';
 import './App.scss';
 
 const App: React.FC = () => {
@@ -17,9 +17,8 @@ const App: React.FC = () => {
 			<div ref={divRef}>
 				{isLoaded ? (
 					<div className={'particle-sim-canvas'}>
-						<ParticleSimulator
+						<NbodySimulator
 							parentRef={divRef}
-							nbodyCountComputer={10}
 							// initColor={[0xFF, 0x4C, 0x19, 0x80]}
 							// finalColor={[0xFF, 0xFF, 0xFF, 0xFF]}
 						/>
