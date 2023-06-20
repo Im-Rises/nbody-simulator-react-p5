dc81358f36a09081af792224ca994a1b93153980
Im-Rises <quentin-morel88@hotmail.com>
2023-06-20 19:22:39 -0400

    New version

--- src/constants/constant-nbody-simulator.ts
+++ src/constants/constant-nbody-simulator.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-const NBODY_COUNT_COMPUTER = 30;
-const NBODY_COUNT_MOBILE = 10;
+const NBODY_COUNT_COMPUTER = 100;
+const NBODY_COUNT_MOBILE = 50;
 
 export {NBODY_COUNT_COMPUTER, NBODY_COUNT_MOBILE};
