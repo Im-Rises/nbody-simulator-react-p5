3550f79638ce1693f1d416cc9d6afe5bc0581f32
Im-Rises <quentin-morel88@hotmail.com>
2023-09-02 23:44:01 -0400

    wip

--- src/classes/Quadtree.ts
+++ src/classes/Quadtree.ts
@@ -30,12 +30,12 @@ class Rectangle {
             point.y <= this.y + this.h);
     }
 
-    // intersects(range: Rectangle) {
-    //     return !(range.x - range.w > this.x + this.w ||
-    //         range.x + range.w < this.x - this.w ||
-    //         range.y - range.h > this.y + this.h ||
-    //         range.y + range.h < this.y - this.h);
-    // }
+    intersects(range: Rectangle) {
+        return !(range.x - range.w > this.x + this.w ||
+            range.x + range.w < this.x - this.w ||
+            range.y - range.h > this.y + this.h ||
+            range.y + range.h < this.y - this.h);
+    }
 }
 
 class Quadtree {
@@ -104,13 +104,31 @@ class Quadtree {
         const y = this.boundary.y;
         const w = this.boundary.w;
         const h = this.boundary.h;
-        
+
         this.northWest = new Quadtree(new Rectangle(x, y, w / 2, h / 2), this.capacity);
         this.northEast = new Quadtree(new Rectangle(x + w / 2, y, w / 2, h / 2), this.capacity);
         this.southWest = new Quadtree(new Rectangle(x, y + h / 2, w / 2, h / 2), this.capacity);
         this.southEast = new Quadtree(new Rectangle(x + w / 2, y + h / 2, w / 2, h / 2), this.capacity);
 
         this.divided = true;
+    }
+
+    query(range: Rectangle, found: Point[]) {
+        if (!this.boundary.intersects(range)) {
+            return;
+        } else {
+            for (const p of this.points) {
+                if (range.contains(p)) {
+                    found.push(p);
+                }
+            }
+            if (this.divided) {
+                this.northWest.query(range, found);
+                this.northEast.query(range, found);
+                this.southWest.query(range, found);
+                this.southEast.query(range, found);
+            }
+        }
     }
 
     show(p5: p5Types) {
--- src/components/NbodySimulatorBarnesHut.tsx
+++ src/components/NbodySimulatorBarnesHut.tsx
@@ -22,16 +22,34 @@ const NbodySimulatorBarnesHut = (props: Props) => {
         console.log(quadtree.insert(points[i]));
     }
 
-    console.log(quadtree);
+    // console.log(quadtree);
+
+    // Intersecting rectangle
+    const range = new Rectangle(250, 250, 100, 100);
+    const pointsIntIntersection = [];
+    quadtree.query(range, pointsIntIntersection);
+    console.log(pointsIntIntersection);
 
 
     const setup = (p5: p5Types, canvasParentRef: Element) => {
         p5.createCanvas(widthHeight, widthHeight).parent(canvasParentRef);
     };
 
+
     const draw = (p5: p5Types) => {
         p5.background(100);
         quadtree.show(p5);
+
+        // Draw the rectangle of intersection
+        p5.strokeWeight(1);
+        p5.stroke(0, 255, 0);
+        p5.rect(range.x - range.w, range.y - range.h, range.w * 2, range.h * 2);
+        // Draw all the points which are in the intersection
+        for (const p of pointsIntIntersection) {
+            p5.strokeWeight(4);
+            p5.stroke(0, 255, 0);
+            p5.point(p.x, p.y);
+        }
     };
 
     return <Sketch setup={setup} draw={draw}/>;
